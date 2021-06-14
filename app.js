d306cfddf667847468ff6847b24e3d1777ce2d88
K.Kimura <dotnsf@gmail.com>
2021-06-15 02:22:20 +0900

    no access_token

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 //. app.js
 
 var express = require( 'express' ),
-    crypto = require( 'crypto' ),
-    multer = require( 'multer' ),
-    bodyParser = require( 'body-parser' ),
-    fs = require( 'fs' ),
     ejs = require( 'ejs' ),
     passport = require( 'passport' ),
-    request = require( 'request' ),
     session = require( 'express-session' ),
     WebAppStrategy = require( 'ibmcloud-appid' ).WebAppStrategy,
     app = express();
@@ -35,24 +30,12 @@ passport.use( new WebAppStrategy({
   redirectUri: settings.redirectUri
 }));
 
-//. #35
-var access_token = null;
-getAccessToken().then( function( token ){
-  if( token ){
-    access_token = token;
-  }
-}).catch( function( err ){
-  console.log( err );
-});
-
-app.use( bodyParser.urlencoded( { extended: true } ) );
-app.use( bodyParser.json() );
+//. enable routing
 app.use( express.Router() );
-app.use( express.static( __dirname + '/public' ) );
 
+//. template engine
 app.set( 'views', __dirname + '/views' );
 app.set( 'view engine', 'ejs' );
-
 
 //. login
 app.get( '/appid/login', passport.authenticate( WebAppStrategy.STRATEGY_NAME, {
@@ -72,31 +55,7 @@ app.get( '/appid/logout', function( req, res ){
   res.redirect( '/' );
 });
 
-//. get user info
-app.get( '/appid/user', async function( req, res ){
-  //console.log( req.user );
-  if( !req.user || !req.user.sub ){
-    res.status( 401 );
-    res.send( '' );
-  }else{
-    var profile = await getProfile( req.user.sub );
-    //console.log( profile );
-    if( profile && profile.profile ){
-      res.json({
-        user: {
-          id: req.user.sub,
-          name: req.user.name,
-          email: req.user.email,
-          attributes: profile.profile.attributes
-        }
-      });
-    }else{
-      res.status( 401 );
-      res.send( '' );
-    }
-  }
-});
-
+//. access restriction
 app.all( '/*', function( req, res, next ){
   if( !req.user || !req.user.sub ){
     res.redirect( '/appid/login' );
@@ -106,87 +65,13 @@ app.all( '/*', function( req, res, next ){
 });
 
 
-app.get( '/', async function( req, res ){
-  //console.log( req.user ); //. { name: 'FX', email: 'fx@teyan.de' }
-  var profile = await getProfile( req.user.sub );
-  //console.log( profile.profile ); //. { id: '28..', name: 'FX', email: 'fx@teyan.de' }
-  res.render( 'index', { profile: profile.profile } );
+//. top page
+app.get( '/', function( req, res ){
+  res.render( 'index', { profile: req.user } );
 });
 
-function timestamp2datetime( ts ){
-  if( ts ){
-    var dt = new Date( ts );
-    var yyyy = dt.getFullYear();
-    var mm = dt.getMonth() + 1;
-    var dd = dt.getDate();
-    var hh = dt.getHours();
-    var nn = dt.getMinutes();
-    var ss = dt.getSeconds();
-    var datetime = yyyy + '-' + ( mm < 10 ? '0' : '' ) + mm + '-' + ( dd < 10 ? '0' : '' ) + dd
-      + ' ' + ( hh < 10 ? '0' : '' ) + hh + ':' + ( nn < 10 ? '0' : '' ) + nn + ':' + ( ss < 10 ? '0' : '' ) + ss;
-    return datetime;
-  }else{
-    return "";
-  }
-}
 
-async function getProfile( userId ){
-  return new Promise( async ( resolve, reject ) => {
-    if( access_token ){
-      var headers1 = {
-        accept: 'application/json',
-        authorization: 'Bearer ' + access_token
-      };
-      var option1 = {
-        url: 'https://' + settings.region + '.appid.cloud.ibm.com/management/v4/' + settings.tenantId + '/users/' + userId + '/profile',
-        method: 'GET',
-        headers: headers1
-      };
-      request( option1, ( err1, res1, body1 ) => {
-        if( err1 ){
-          console.log( 'err1', err1 );
-          resolve( null );
-        }else{
-          var profile = JSON.parse( body1 );
-          //console.log( JSON.stringify( profile, null, 2 ) );
-          resolve( { status: true, profile: profile } );
-        }
-      });
-    }else{
-      resolve( null );
-    }
-  });
-}
-
-async function getAccessToken(){
-  return new Promise( async ( resolve, reject ) => {
-    //. GET an IAM token
-    //. https://cloud.ibm.com/docs/appid?topic=appid-manging-api&locale=ja
-    var headers = {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Accept': 'application/json'
-    };
-    var option = {
-      url: 'https://iam.cloud.ibm.com/oidc/token',
-      method: 'POST',
-      body: 'grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=' + settings.apiKey,
-      headers: headers
-    };
-    request( option, ( err, res, body ) => {
-      if( err ){
-        console.log( err );
-        resolve( null );
-      }else{
-        body = JSON.parse( body );
-        var access_token = body.access_token;
-        resolve( access_token );
-      }
-    });
-  });
-}
-
-
-//app.listen( appEnv.port );
+//. listening to port
 var port = process.env.PORT || 8080;
 app.listen( port );
 console.log( "server starting on " + port + " ..." );
